5d7c7d6044075ad2252baec15601a9e728ae7e3f
bullettoothaxe <bullettoothaxe@gmail.com>
2022-12-24 19:58:59 +0200

    check

--- main.py
+++ main.py
@@ -5,11 +5,28 @@ import time
 has_light_message = "💡😺💡"
 no_light_message = "🕯️🙅‍♀️🕯️"
 
+users = []
+
+
+def uniq(items):
+    return list(set(items))
+
+
+def get_subscribers(from_updates):
+    global users
+
+    for user in from_updates:
+        if user not in users:
+            users.append(user)
+
+    return users
+
 
 def send_notification(status: bool):
     message = has_light_message if status else no_light_message
     updates = bot.get_updates()
-    chat_ids = list(set([update.message.chat.id for update in updates]))
+    from_updates = uniq([update.message.chat.id for update in updates])
+    chat_ids = get_subscribers(from_updates)
     print(chat_ids)
 
     for chat_id in chat_ids:
