81310466517463b7846e2c15d982a3ddd1944d05
bullettoothaxe <bullettoothaxe@gmail.com>
2023-01-13 20:37:16 +0200

    users count

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
 import telebot
 from env import BOT_TOKEN
+import users
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['users_anal'])
+def schedule_handler(message):
+    chat_id = message.chat.id
+    active_users = users.read()
+    message = f"Users count: {len(active_users)}"
+    bot.send_message(chat_id, message)
