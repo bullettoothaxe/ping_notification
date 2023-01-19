f11bee1f2466affdc6c5489a7e6b5811bc8a2938
Oleksii Goiman <ogoiman@evolvtechnology.com>
2023-01-19 11:45:31 +0200

    rename

--- bot.py
+++ bot.py
@@ -5,15 +5,15 @@ import users
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    chat_id = message.chat.id
+    users.add_user(chat_id)
+
+
 @bot.message_handler(commands=['users_anal'])
 def users_anal_handler(message):
     chat_id = message.chat.id
     active_users = users.read()
     message = f"Users count: {len(active_users)}"
     bot.send_message(chat_id, message)
-
-
-@bot.message_handler(commands=['start'])
-def start_handler(message):
-    chat_id = message.chat.id
-    users.add_user(chat_id)
