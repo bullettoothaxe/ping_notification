6b169db367efc64161cd4ad4ba8bf97664e309e4
bullettoothaxe <bullettoothaxe@gmail.com>
2023-01-21 09:38:08 +0200

    upd

--- bot.py
+++ bot.py
@@ -1,5 +1,19 @@
 import telebot
 from env import BOT_TOKEN
+import users
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    chat_id = message.chat.id
+    users.add_user(chat_id)
+
+
+@bot.message_handler(commands=['users_anal'])
+def users_anal_handler(message):
+    chat_id = message.chat.id
+    active_users = users.read()
+    message = f"Users count: {len(active_users)}"
+    bot.send_message(chat_id, message)
